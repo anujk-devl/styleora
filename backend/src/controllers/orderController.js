1aff05ba9679beb6d3d2a367e369bb16fefc882e
Anuj Yadav <anujyadav@example.com>
2025-11-25 08:27:35 +0530

    *

--- backend/src/controllers/orderController.js
+++ backend/src/controllers/orderController.js
@@ -1,15 +1,18 @@
 import Order from "../models/Order.js";
 
+
 export const createOrder = async (req, res) => {
   try {
     const { items, totalAmount } = req.body;
     if (!items?.length) return res.status(400).json({ error: "Cart is empty" });
+
     const order = await Order.create({
-      user: req.user._id,
+      user: req.user._id, 
       items,
       totalAmount,
       status: "paid"
     });
+
     res.status(201).json(order);
   } catch (err) {
     console.error("Create order error:", err);
@@ -17,11 +20,8 @@ export const createOrder = async (req, res) => {
   }
 };
 
-
 export const getMyOrders = async (req, res) => {
-  const orders = await Order.find({ user: req.user.id }).populate(
-    "items.product",
-    "name price images"
-  );
+  const orders = await Order.find({ user: req.user._id }) 
+    .populate("items.product", "name price images");
   res.json(orders);
 };
